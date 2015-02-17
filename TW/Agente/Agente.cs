78eb1a959b4820834534881498c3d999858da2b5
Christian Lucero <crizlucero@gmail.com>
2015-02-17 11:46:33 -0600

    Primera versión

---
+++ TW/Agente/Agente.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace TW.Agente
+{
+    class Agente
+    {
+        private Grid edoInicial { get; set; }
+        private Grid edoActual { get; set; }
+        private int[] SelBloque = new int[2];
+
+        public Agente(Grid Entorno)
+        {
+            this.edoInicial = Entorno;
+            this.edoActual = Entorno;
+        }
+
+        private void BuscarBloqueCercano()
+        {
+            for (int i = 4; i < 8; i++)
+            {
+            }
+        }
+        /// <summary>
+        /// Mueve el agente de un lado a otro
+        /// </summary>
+        /// <param name="Entorno">Entorno de donde está el agente</param>
+        /// <returns>Si es posible moverse</returns>
+        public void MoverAgente(Grid Entorno)
+        {
+            this.edoActual = Entorno;
+
+            int x = Grid.GetColumn(this.edoActual.Children[0]);
+            int y = Grid.GetRow(this.edoActual.Children[0]);
+            switch (this.SeleccionarMovimiento())
+            {
+                case 1: this.MoverArriba(x, --y); break;
+                case 2: this.MoverAbajo(x, ++y); break;
+                case 3: this.MoverIzquierda(--x, y); break;
+                case 4: this.MoverDerecha(++x, y); break;
+            }
+            return;
+            //Reglas
+            //Arriba
+            if (y != 0)
+            {
+                this.MoverArriba(x, --y);
+            }
+            //Abajo
+            else if (y != 7)
+            {
+                this.MoverAbajo(x, ++y);
+            }
+            //Izquierda
+            else if (x != 0)
+            {
+                this.MoverIzquierda(--x, y);
+            }
+            //Derecha
+            else if (x != 7)
+            {
+                this.MoverDerecha(++x, y);
+            }
+        }
+
+        private void SeleccionarBloque()
+        {
+            int ax = Grid.GetColumn(this.edoActual.Children[0]);
+            int ay = Grid.GetRow(this.edoActual.Children[0]);
+
+            int bx = int.MaxValue;
+            int by = int.MaxValue;
+
+            foreach (var bloque in this.edoActual.Children)
+            {
+                if (bloque is Rectangle)
+                {
+                    Rectangle block = (Rectangle)bloque;
+                    if (block.Fill.ToString() == Colors.Gray.ToString())
+                    {
+                        if (Math.Abs((Grid.GetColumn(block) + Grid.GetRow(block)) - (ax + ay)) < Math.Abs((bx + by) - (ax + ay)))
+                        {
+                            bx = Grid.GetColumn(block);
+                            by = Grid.GetRow(block);
+                            this.SelBloque[0] = bx;
+                            this.SelBloque[1] = by;
+                        }
+                    }
+                }
+            }
+        }
+
+        private int SeleccionarMovimiento()
+        {
+            this.SeleccionarBloque();
+            int ax = Grid.GetColumn(this.edoActual.Children[0]);
+            int ay = Grid.GetRow(this.edoActual.Children[0]);
+            //Movimientos horizontales
+            if ((Math.Abs(this.SelBloque[0] - ax) < Math.Abs(this.SelBloque[1] - ay)) && this.SelBloque[0] - ax != 0)
+            {
+                //Mover a la derecha
+                if (this.SelBloque[0] - ax > 0)
+                {
+                    if (ax != 7) return 4;
+                    else if (ay != 0) return 2;
+                    else if (ay != 7) return 1;
+                    else return 3;
+                }
+                else //Mover a la izquierda
+                {
+                    if (ax != 0) return 3;
+                    else if (ay != 0) return 2;
+                    else if (ay != 7) return 1;
+                    else return 4;
+                }
+            }
+            else //Movimientos verticales
+            {
+                //Mover para abajo
+                if (this.SelBloque[1] - ay > 0)
+                {
+                    if (ay != 7) return 2;
+                    else if (ax != 0) return 3;
+                    else if (ax != 7) return 4;
+                    return 1;
+                }
+                else //Mover para arriba
+                {
+                    if (ay != 0) return 1;
+                    else if (ax != 0) return 3;
+                    else if (ax != 7) return 4;
+                    else return 2;
+                }
+            }
+            return 0;
+        }
+        /// <summary>
+        /// Movimiento hacia arriba del agente.
+        /// Si puede mover un bloque, lo hace.
+        /// </summary>
+        private void MoverArriba(int x, int y)
+        {
+            var elemento = this.edoActual.Children.Cast<UIElement>().FirstOrDefault(i => Grid.GetColumn(i) == x && Grid.GetRow(i) == y);
+            if (elemento is Rectangle)
+            {
+                if (((Rectangle)elemento).Fill.ToString() == Colors.Gray.ToString())
+                {
+                    if (Grid.GetRow(elemento) != 0)
+                    {
+                        //Mover bloque
+                        Grid.SetRow(elemento, y);
+                        Grid.SetRow(this.edoActual.Children[0], y);
+                    }
+                }
+            }
+            else
+            {
+                //Se moverá el agente
+                Grid.SetRow(this.edoActual.Children[0], y);
+            }
+        }
+        /// <summary>
+        /// Movimiento hacia abajo del agente.
+        /// Si puede mover un bloque, lo hace.
+        /// </summary>
+        private void MoverAbajo(int x, int y)
+        {
+            var elemento = this.edoActual.Children.Cast<UIElement>().FirstOrDefault(i => Grid.GetColumn(i) == x && Grid.GetRow(i) == y);
+            if (elemento is Rectangle)
+            {
+                if (((Rectangle)elemento).Fill.ToString() == Colors.Gray.ToString())
+                {
+                    if (Grid.GetRow(elemento) != 0)
+                    {
+                        //Mover bloque
+                        Grid.SetRow(elemento, y);
+                        Grid.SetRow(this.edoActual.Children[0], y);
+                    }
+                }
+            }
+            else
+            {
+                //Se moverá el agente
+                Grid.SetRow(this.edoActual.Children[0], y);
+            }
+        }
+        /// <summary>
+        /// Movimiento hacia la derecha del agente.
+        /// Si puede mover un bloque, lo hace.
+        /// </summary>
+        private void MoverDerecha(int x, int y)
+        {
+            var elemento = this.edoActual.Children.Cast<UIElement>().FirstOrDefault(i => Grid.GetColumn(i) == x && Grid.GetRow(i) == y);
+            if (elemento is Rectangle)
+            {
+                if (((Rectangle)elemento).Fill.ToString() == Colors.Gray.ToString())
+                {
+                    if (Grid.GetColumn(elemento) != 7)
+                    {
+                        //Mover bloque
+                        Grid.SetColumn(elemento, x);
+                        Grid.SetColumn(this.edoActual.Children[0], x);
+                    }
+                }
+            }
+            else
+            {
+                //Se moverá el agente
+                Grid.SetColumn(this.edoActual.Children[0], x);
+            }
+        }
+        /// <summary>
+        /// Movimiento hacia la izquierda del agente.
+        /// Si puede mover un bloque, lo hace.
+        /// </summary>
+        private void MoverIzquierda(int x, int y)
+        {
+            var elemento = this.edoActual.Children.Cast<UIElement>().FirstOrDefault(i => Grid.GetColumn(i) == x && Grid.GetRow(i) == y);
+            if (elemento is Rectangle)
+            {
+                if (((Rectangle)elemento).Fill.ToString() == Colors.Gray.ToString())
+                {
+                    if (Grid.GetColumn(elemento) != 7)
+                    {
+                        //Mover bloque
+                        Grid.SetColumn(elemento, x);
+                        Grid.SetColumn(this.edoActual.Children[0], x);
+                    }
+                }
+            }
+            else
+            {
+                //Se moverá el agente
+                Grid.SetColumn(this.edoActual.Children[0], x);
+            }
+        }
+    }
+}
---
+++ TW/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TW
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<int[]> posiciones = new List<int[]>();
+        private Agente.Agente Jugador;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.AddAgente();
+            this.AddElementos(Colors.Black, 5);
+            this.AddElementos(Colors.Gray, 5);
+            this.AddElementos(Colors.LightGray, 10);
+
+            /*this.Jugador = new Agente.Agente(Entorno);
+            if (!this.Jugador.ValidarEntorno())
+            {
+                MessageBox.Show("El entorno no es viable para funcionar");
+                Application.Current.Shutdown();
+            }*/
+            this.Jugador = new Agente.Agente(Entorno);
+        }
+
+        public void AddAgente()
+        {
+            Agente.Width = 25;
+            Agente.Height = 25;
+            Random rand = new Random();
+
+            int[] xy = new int[2];
+            xy[0] = rand.Next(0, 8);
+            xy[1] = rand.Next(0, 8);
+            Grid.SetRow(Agente, xy[0]);
+            Grid.SetColumn(Agente, xy[1]);
+            this.posiciones.Add(xy);
+        }
+        /// <summary>
+        /// Agrega los elementos de pared, bloque y agujeros al entorno
+        /// </summary>
+        /// <param name="color">Color del elemento</param>
+        /// <param name="cantidad">Cantidad de elementos que se generarán</param>
+        public void AddElementos(Color color, int cantidad)
+        {
+            Random rand = new Random();
+            int total = rand.Next(1, cantidad);
+            int x = rand.Next(0, 8);
+            int y = rand.Next(0, 8);
+            for (int i = 0; i < total; i++)
+            {
+                Rectangle r = new Rectangle
+                {
+                    Fill = new SolidColorBrush(color)
+                };
+                var valor = this.Entorno.Children.Cast<UIElement>().FirstOrDefault(e => Grid.GetColumn(e) == x && Grid.GetRow(e) == y);
+                if (!(valor is Rectangle) && !(valor is Image))
+                {
+                    this.Entorno.Children.Add(r);
+                    r.SetValue(Grid.ColumnProperty, x);
+                    r.SetValue(Grid.RowProperty, y);
+                }
+                else
+                {
+                    i--;
+                }
+                x = rand.Next(0, 8);
+                y = rand.Next(0, 8);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Jugador.MoverAgente(Entorno);
+        }
+    }
+}
